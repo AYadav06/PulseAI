dd94c30e3159760dab661c53ffb152c6622fb5a6
Amit Yadav <amity7205@gmail.com>
2026-07-27 00:56:29 +0530

    add:cd file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,6 @@ dotenv.config();
 
 const app=express();
 const port=3000;
-console.log("DEBUG: GEMINI_API_KEY is:", process.env.GEMINI_API_KEY ? "FOUND (Starts with " + process.env.GEMINI_API_KEY.slice(0, 5) + "...)" : "NOT FOUND / UNDEFINED");
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
@@ -25,7 +24,6 @@ app.listen(port ,()=>{
     console.log("server is running...");
 })
 
-// Place this at the VERY END of your server.ts routes
 app.use((req, res) => {
     console.log(`Received a ${req.method} request to path: ${req.originalUrl}`);
     res.status(404).send(`Route not found on this server.`);
