dd2849aa1afc6d13d03595bcb506b05a90c8fde9
Amit Yadav <amity7205@gmail.com>
2026-07-27 11:40:04 +0530

    fix:cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,9 +12,29 @@ dotenv.config();
 
 const app=express();
 const port=3000;
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
+
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL, 
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/v1",userRouter);
 app.use("/api/v1",chatRouter);
